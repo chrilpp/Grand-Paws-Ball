dbdd0153941cc5b5d27de18d09556cddae7f2250
Jesper Hellberg <Caegrim@users.noreply.github.com>
2018-02-18 00:12:46 +0100

    Added script folder and began playerController script

---
+++ GrandPawsBall/Assets/Scrips/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerController : MonoBehaviour {
+
+	Vector3 mp = new Vector3();
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) 
+			Debug.Log ("Pressed left click.");
+			
+		if (Input.GetMouseButtonDown(1))
+			Debug.Log("Pressed right click.");
+
+		if (Input.GetMouseButtonDown(2))
+			Debug.Log("Pressed middle click.");
+	}
+}
