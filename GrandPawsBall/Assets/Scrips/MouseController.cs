31f7a61b0716a747c34d7597bfaab8c55ffc35ad
Christian Thulin Pfeifer <christian.thpf@gmail.com>
2018-02-19 20:47:14 +0100

    mouseController + CameraLookAt + Free Standard Assets

---
+++ GrandPawsBall/Assets/Scrips/MouseController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+	//Instance Variables
+	Vector3 targetPosition;
+	Vector3 lookAtTarget;
+	Quaternion playerRot;
+	float rotSpeed = 5;
+	float speed = 10;
+	bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Checkes if the player has pressed the left mouse button//
+		if (Input.GetMouseButton(0)) {
+			setTargetPosition();
+		}
+		if (moving) {
+			Move ();
+		}
+	}
+
+	void setTargetPosition(){
+		//Creates an raycast from where the mouse points through the main camera//
+		Ray myRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+						//räknar ut punkten genom att ge rayen, hitpointen och hur långt bort det max får vara.
+		if (Physics.Raycast(myRay, out hit, 1000)){
+			targetPosition = hit.point;
+			lookAtTarget = new Vector3(targetPosition.x - transform.position.x,
+				transform.position.y,
+				targetPosition.z - transform.position.z);
+
+			//Creates the rotation variable from the new point
+			playerRot = Quaternion.LookRotation(lookAtTarget);
+				moving = true;
+		
+	}
+			
+}
+
+	void Move(){
+
+		transform.rotation = Quaternion.Slerp (transform.rotation, playerRot, rotSpeed * Time.deltaTime);
+		transform.position = Vector3.MoveTowards (transform.position, targetPosition, speed * Time.deltaTime);
+
+		float  distance = Vector3.Distance(transform.position, targetPosition);
+		if (distance <= 0.1F)
+		{
+			moving = false;
+		}
+
+
+	}
+}
---
+++ GrandPawsBall/Assets/Scrips/MouseController2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController2 : MonoBehaviour {
+
+	public float speed = 10f;
+	public Vector3 targetPos;
+	public bool isMoving;
+	const int MOUSE = 0;
+	// Use this for initialization1
+	void Start () {
+
+		targetPos = transform.position;
+		isMoving = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButton(MOUSE))
+		{
+			SetTarggetPosition();
+		}
+		if(isMoving)
+		{
+			MoveObject();
+		}
+	}
+	void SetTarggetPosition()
+	{
+		Plane plane = new Plane(Vector3.up,transform.position);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float point = 0f;
+
+		if(plane.Raycast(ray, out point))
+			targetPos = ray.GetPoint(point);
+
+		isMoving = true;
+	}
+	void MoveObject()
+	{
+		transform.LookAt(targetPos);
+		transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+		if (transform.position == targetPos)
+			isMoving = false;
+		Debug.DrawLine(transform.position,targetPos,Color.red);
+
+	}
+}
